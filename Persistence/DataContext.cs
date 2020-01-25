4a852d1843341055aaafc7daa5a356bf4c6b0496
sotomaque <sotomaque@gmail.com>
2020-01-25 15:52:15 -0800

    added references to projects

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFramework.DbContext;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DBSet<Value> Values { get; set; }
+    }
+}
